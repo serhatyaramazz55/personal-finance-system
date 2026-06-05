b2a85217ab65542379fbe07c6e4215361533ac2a
Serhat Yaramaz <biesyi.2222816@gmail.com>
2026-06-05 13:33:47 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PersonalFinanceSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Personal Finance Management System");
+        }
+    }
+}
